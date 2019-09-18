6f8a9dcd2d7024a7246dd07cb01cd2e0db566c31
cesarz <cesarz@ksu.edu>
2019-09-18 17:14:54 -0500

    wip

---
+++ Menu/Sides/Fryceritops.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    /// <summary>
+    /// Manages the Fryceritops side.
+    /// </summary>
+    public class Fryceritops : Side
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Fryceritops()
+        {
+            this.ingredients.Add("Potatoes");
+            this.ingredients.Add("Salt");
+            this.ingredients.Add("Vegetable Oil");
+        }
+
+        /// <summary>
+        /// Overrides the base size method.
+        /// </summary>
+        public override Size Size
+        {
+            get => this.Size;
+
+            set
+            {
+                Size = value;
+                switch (this.Size)
+                {
+                    case Size.Large:
+                        this.Price = 1.95;
+                        this.Calories = 480;
+                        break;
+                    case Size.Medium:
+                        this.Price = 1.45;
+                        this.Calories = 365;
+                        break;
+                    case Size.Small:
+                        this.Price = 0.99;
+                        this.Calories = 222;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Menu/Sides/MeteorMacAndCheese.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    /// <summary>
+    /// Manages the Meteor MnC side.
+    /// </summary>
+    public class MeteorMacAndCheese : Side
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public MeteorMacAndCheese()
+        {
+            this.ingredients.Add("Macaroni Noodles");
+            this.ingredients.Add("Cheese Product");
+            this.ingredients.Add("Pork Sausage");
+        }
+
+        /// <summary>
+        /// Overrides the base size method.
+        /// </summary>
+        public override Size Size
+        {
+            get => this.Size;
+            set
+            {
+                this.Size = value;
+                switch (this.Size)
+                {
+                    case Size.Large:
+                        this.Price = 1.95;
+                        this.Calories = 520;
+                        break;
+                    case Size.Medium:
+                        this.Price = 1.45;
+                        this.Calories = 490;
+                        break;
+                    case Size.Small:
+                        this.Price = 0.99;
+                        this.Calories = 420;
+                        break;
+                }
+            }
+        }
+    }
+}
