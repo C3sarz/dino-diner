1c7217ff7ede06c29fbc41f76b01ea0263049f02
Cesar Zavala <cesarz@ksu.edu>
2019-09-26 18:33:07 -0500

    .

---
+++ Menu/Drinks/Drink.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Drinks
+{
+    public abstract class Drink
+    {
+        /// <summary>
+        /// Keeps the size.
+        /// </summary>
+        protected Size size { get; set; }
+
+        /// <summary>
+        /// Gets and sets the price
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Gets and sets the calories
+        /// </summary>
+        public uint Calories { get; set; }
+
+        /// <summary>
+        /// Gets the ingredients list
+        /// </summary>
+        public abstract List<string> Ingredients { get; }
+
+        /// <summary>
+        /// Gets or sets the size
+        /// </summary>
+        public virtual Size Size { get; set; }
+    }
+}
--- Menu/Drinks/Tyrannotea.cs
+++ Menu/Drinks/Tyrannotea.cs
@@ -2,28 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
-
 namespace DinoDiner.Menu.Drinks
 {
-    public class Sodasaurus : Drink
+    public class Tyrannotea : Drink
     {
-        /// <summary>
-        /// Keeps the flavor.
-        /// </summary>
-        private SodasaurusFlavor flavor;
-
-        /// <summary>
-        /// Public field for the flavor.
-        /// </summary>
-        public SodasaurusFlavor Flavor {
-            get { return flavor; }
-            set { flavor = value; }
-        }
-
         /// <summary>
         /// Constructor.
         /// </summary>
-        public Sodasaurus()
+        public Tyrannotea()
         {
             this.Size = Size.Small;
         }
@@ -31,12 +17,13 @@ namespace DinoDiner.Menu.Drinks
         /// <summary>
         /// Manages size gets and sets.
         /// </summary>
-        public override Size Size {
+        public override Size Size
+        {
             get { return size; }
             set
             {
                 size = value;
-                if(size == Size.Small)
+                if (size == Size.Small)
                 {
                     Price = 1.50;
                     Calories = 112;
@@ -58,18 +45,7 @@ namespace DinoDiner.Menu.Drinks
         /// <summary>
         /// Holds the ingredients
         /// </summary>
-        public override List<string> Ingredients { get; } = new List<string> { "Water", "Natural Flavors", "Cane Sugar" };
-
-
-        /// <summary>
-        /// Manages ice (default true).
-        /// </summary>
-        public bool Ice { get; set; } = true;
+        public override List<string> Ingredients { get; } = new List<string> { "Water", "Tea", "Cane Sugar" };
 
     }
 }
-
-
-//Property vs field (for the test)
-//define argument
-//camelcase used for private and protected
